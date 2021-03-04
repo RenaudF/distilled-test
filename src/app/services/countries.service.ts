7eaf1f2cf3801f8facb6c5b1263b3458dbb0145b
RenaudF <sirgzu@hotmail.com>
2021-03-04 14:10:49 +0000

    country service can now get individual countries

--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { shareReplay } from 'rxjs/operators';
+import { filter, map, shareReplay } from 'rxjs/operators';
 import { Country } from '../models/country';
+import { Observable } from 'rxjs';
+
+const isDefined = <T>(test: T | undefined): test is T => {
+  return test !== undefined;
+};
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +15,16 @@ export class CountriesService {
   public countries$ = this.http
     .get<Country[]>('https://restcountries.eu/rest/v2/all')
     .pipe(shareReplay(1));
+
+  getCountry$(alpha3Code: string): Observable<Country> {
+    return this.countries$.pipe(
+      map((countries) =>
+        countries.find((country) => country.alpha3Code === alpha3Code)
+      ),
+      filter(isDefined),
+      shareReplay(1)
+    );
+  }
 
   constructor(private http: HttpClient) {}
 }
