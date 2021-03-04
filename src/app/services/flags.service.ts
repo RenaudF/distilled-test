04f24585db79eceefd352d63c9c459d9e6a3bb35
RenaudF <sirgzu@hotmail.com>
2021-03-04 14:13:51 +0000

    flag service now load flags lazily

--- src/app/services/flags.service.ts
+++ src/app/services/flags.service.ts
@@ -2,7 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {
+  distinctUntilChanged,
+  map,
+  shareReplay,
+  switchMap,
+} from 'rxjs/operators';
 import { CountriesService } from './countries.service';
 
 const sanitize = (sanitizer: DomSanitizer) => (svg: string) => {
@@ -15,16 +20,21 @@ const sanitize = (sanitizer: DomSanitizer) => (svg: string) => {
   providedIn: 'root',
 })
 export class FlagsService {
-  public flag$Map$ = this.countriesService.countries$.pipe(
-    map((countries) =>
-      countries.reduce((acc, country) => {
-        acc[country.alpha3Code] = this.http
-          .get(country.flag, { responseType: 'text' })
-          .pipe(map(sanitize(this.sanitizer)), shareReplay(1));
-        return acc;
-      }, {} as Record<string, Observable<SafeResourceUrl>>)
-    )
-  );
+  private cache: Record<string, Observable<SafeResourceUrl>> = {};
+
+  getFlag$(alpha3Code: string): Observable<SafeResourceUrl> {
+    this.cache[alpha3Code] =
+      this.cache[alpha3Code] ||
+      this.countriesService.getCountry$(alpha3Code).pipe(
+        distinctUntilChanged(),
+        switchMap((country) =>
+          this.http.get(country.flag, { responseType: 'text' })
+        ),
+        map(sanitize(this.sanitizer)),
+        shareReplay(1)
+      );
+    return this.cache[alpha3Code];
+  }
 
   constructor(
     private countriesService: CountriesService,
